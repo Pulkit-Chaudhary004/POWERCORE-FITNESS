d43e2b177ca15bd5c65bfeb65e7bfaae52766d06
Pulkit-Chaudhary004 <165458069+Pulkit-Chaudhary004@users.noreply.github.com>
2026-04-13 15:42:15 +0530

    first commit

---
+++ src/Components/TrainWithUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrainWithUs = () => {
+  return (
+  <>
+    <div className="container-fluid fs-1 my-5">train with us</div>
+  </>
+  )
+}
+
+export default TrainWithUs

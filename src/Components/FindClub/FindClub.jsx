0eb669ad518d9989e85bd0432cd6a2e0d8c87a63
Pulkit-Chaudhary004 <165458069+Pulkit-Chaudhary004@users.noreply.github.com>
2026-04-30 12:41:42 +0530

    removed duplicate src and public folders and cleaned file structure

--- src/Components/FindClub/FindClub.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FindClub = () => {
-  return (
-    <div className="" style={{marginTop:"10vh"}}>HELLO THIS IS CLUB FINDER</div>
-  )
-}
-
-export default FindClub
